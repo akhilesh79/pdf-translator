935956971d6562e7c5c90f40219cd1d3c96aa1cd
Akhilesh Kumar Mishra <makmishra.99@gmail.com>
2026-04-20 18:17:19 +0530

    fix: change root route handler to use GET method for server listening log

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '50mb' }));
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
